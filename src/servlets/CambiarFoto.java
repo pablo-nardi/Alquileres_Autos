5abcddc2bd2689a3e207eef512d5581ce0c9df06
pablo-nardi <nardipablo31@gmail.com>
2021-03-24 00:17:46 -0300

    se agrego carga de imagenes y filtrado de usuarios + otros cambios: 	modificado:     WebContent/ABMModelos.jsp 	modificado:     WebContent/ABMUsuario.jsp 	modificado:     WebContent/CambiarFoto.jsp 	borrado:        WebContent/IMAGENES/Modelos/BingWallpaper.jpg 	borrado:        WebContent/IMAGENES/Modelos/WhiteCliffs_1920x1080.png 	nuevo archivo:  WebContent/IMAGENES/Modelos/ferrari.jpg 	nuevo archivo:  WebContent/IMAGENES/Modelos/ford-mondeo (copy).jpg 	nuevo archivo:  WebContent/IMAGENES/Modelos/lambor.jpg 	nuevo archivo:  WebContent/IMAGENES/Modelos/otra-ferrari.jpg 	nuevo archivo:  WebContent/IMAGENES/Modelos/porche.jpg 	nuevo archivo:  WebContent/IMAGENES/Modelos/roja.jpg 	nuevo archivo:  WebContent/IMAGENES/Modelos/testarosa.jpg 	modificado:     WebContent/prueba.jsp 	modificado:     build/classes/.gitignore 	modificado:     build/classes/datos/DatosUsuario.class 	modificado:     build/classes/logic/UsuarioLogic.class 	modificado:     src/datos/DatosUsuario.java 	modificado:     src/logic/UsuarioLogic.java 	modificado:     src/servlets/CambiarFoto.java

--- src/servlets/CambiarFoto.java
+++ src/servlets/CambiarFoto.java
@@ -2,26 +2,39 @@ package servlets;
 
 import java.io.File;
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import entidades.Modelo;
+import logic.ModeloLogic;
+
 @WebServlet({"/CambiarFoto/*"})
+@MultipartConfig(fileSizeThreshold = 6291456, // 6 MB
+maxFileSize = 10485760L, // 10 MB
+maxRequestSize = 20971520L // 20 MB
+)
 public class CambiarFoto extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	ModeloLogic ml;
+	Modelo mod;
        
     public CambiarFoto() {
         super();
+        ml = new ModeloLogic();
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
+			mod = ml.getOne(Integer.parseInt(request.getParameter("id")));
 			switch(request.getPathInfo()) {
 			case "/upload":
-				/*String uploadPath = getServletContext().getRealPath("")  + UPLOAD_DIRECTORY;//+ File.separator
+				//String uploadPath = getServletContext().getRealPath("")  + UPLOAD_DIRECTORY;//+ File.separator
 				String uploadPath = "/home/pablo/git/Alquileres_Autos/WebContent/IMAGENES/Modelos";
 				File uploadDir = new File(uploadPath);
 				if (!uploadDir.exists()) uploadDir.mkdir();
@@ -31,14 +44,15 @@ public class CambiarFoto extends HttpServlet {
 				    part.write(uploadPath + File.separator + fileName);
 				}
 				
-				response.sendRedirect("/Alquileres_Autos/prueba.jsp");*/
+				response.sendRedirect("/Alquileres_Autos/CambiarFoto.jsp");
 				
-				response.sendRedirect("/Alquileres_Autos/prueba.jsp");
+				//response.sendRedirect("/Alquileres_Autos/prueba.jsp");
 				
 				break;
-			case "/select":
-				
-				response.sendRedirect("/Alquileres_Autos/NewFile.jsp");
+			case "/seleccion":
+				mod.setFotoModelo("IMAGENES/Modelos/" + request.getParameter("foto"));
+				ml.update(mod);
+				response.sendRedirect("/Alquileres_Autos/ABMModelos.jsp");
 				break;
 			}
 			
